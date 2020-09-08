950f371537376ff9dd25d58e7dae695336dd30b9
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-08 17:54:29 +0200

    Added styling to the index. No base template that runs a consistent styling to all other pages. Just styling for index.

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,6 +3,9 @@ from django.forms import ModelForm
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Add new task...'
+    }))
     class Meta:
         model = Task
         fields = '__all__'
