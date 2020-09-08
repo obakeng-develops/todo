814c05e28ad6c2678c042487040df391c7fd2abe
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-08 16:54:50 +0200

    Added context content to list template. Ran Task migration. Edited index view to add context dictionary.

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def index(request):
-    return render(request, "tasks/list.html")
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, "tasks/list.html", context)
