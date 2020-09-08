3e4ce165bfa8cd49e7c255d66e92c24d40afe543
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-08 16:48:38 +0200

    Defined Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
