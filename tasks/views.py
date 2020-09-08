ae2ee391b6f290a331c91d822d5faa90cbbdc6ba
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-08 16:59:11 +0200

    Created forms file to create Model Forms and imported the ModelForm as part of the index view

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
     tasks = Task.objects.all()
+
+    form = TaskForm()
+
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'form': form
     }
     return render(request, "tasks/list.html", context)
